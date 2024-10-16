602c2caea89bf59af00eed5851abeccf6a15a217
Gustavo Araujo Santos <gustavo.gustavo-ext@ab-inbev.com>
2024-10-16 13:19:52 -0300

    Initial Commit

---
+++ Snapshot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Snapshot
+//
+//  Created by Gustavo Araujo Santos on 10/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Snapshot/SnapshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapshotApp.swift
+//  Snapshot
+//
+//  Created by Gustavo Araujo Santos on 10/16/24.
+//
+
+import SwiftUI
+
+@main
+struct SnapshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
